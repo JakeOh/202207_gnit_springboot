fa0463a8aae576a833a53c1fa87abfce6e64b6b0
JakeOh <jake.oh.lecture@gmail.com>
2022-07-19 19:51:48 +0900

    0719 #1

---
+++ lab1_java/src/edu/java/review2/Review2Main.java
@@ -0,0 +1,25 @@
+package edu.java.review2;
+
+public class Review2Main {
+
+	public static void main(String[] args) {
+		// 연산자(operator)
+		// 할당 연산자: =
+		// 산술 연산자: +, -, *(곱셈), /(나눗셈), %(나머지)
+		// 정수/정수=몫, 실수/실수=소수점이하 계산.
+		// 복합 할당 연산자: +=, -=, *=, /=, %=, ...
+		// 증감 연산자: ++, --
+		// 비교 연산자: ==, !=, >, >=, <, <=
+		// 논리 연산자: &&(and), ||(or), !(not)
+		
+		// 조건문(conditional statement)
+		int number = 11;
+		if (number % 2 == 0) {
+			System.out.println("짝수");
+		} else {
+			System.out.println("홀수");
+		}
+		
+	}
+
+}
